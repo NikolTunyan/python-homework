9752e5ee88d234a658ab59ea8b66b1d3101e1036
Nikol Tunyan <69039821+NikolTunyan@users.noreply.github.com>
2023-11-01 14:49:58 +0400

    Update main.py

--- lec_2/main.py
+++ lec_2/main.py
@@ -1,14 +1,3 @@
-num1 = int(input("Enter the first integer: "))
-num2 = int(input("Enter the second integer: "))
-print("The sum of",num1,"and",num2,"is:",num1 + num2)
-print("The difference of ",num1," and ",num2," is: ",num1 - num2)
-print("The product of ",num1," and ",num2," is: ",num1 * num2)
-print(num1," / ",num2,"=",num1 / num2)
-print(num1,"//",num2,"=",num1 // num2)
-print(num1,"in the power of",num2,"is:",num1**num2)
-
-
-
 num1 = 12
 num2 = 23.56
 print(num1)
@@ -19,10 +8,6 @@ print("List: ",myList)
 
 myTuple = tuple(myList)
 print("Tuple: ",myTuple)
-
-myDict = {'name':"Mher", 'age' : 20}
-print(myDict)
-
 print("Hello world")
 print("Hello " * 3)
 print("Hello" + " world!")
